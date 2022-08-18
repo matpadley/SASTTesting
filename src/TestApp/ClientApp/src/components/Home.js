65f93ff0d26e1ef2d6f9709daf148bec09738af1
Mathew Padley <git@hewley.co.uk>
2022-08-18 14:55:32 +0100

    sdfsdf

--- src/TestApp/ClientApp/src/components/Home.js
+++ src/TestApp/ClientApp/src/components/Home.js
@@ -7,7 +7,7 @@ export class Home extends Component {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <p>This is mostly nonsense and a bit more stuff</p>
+        <p>This is mostly nonsense and a bit more stuff should still work</p>
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
       </div>
     );
